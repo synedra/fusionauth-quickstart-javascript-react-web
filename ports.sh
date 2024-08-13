31840e4562df2a47af709c51ac9b56661fcd60cf
Kirsten Hunter <77410784+synedra@users.noreply.github.com>
2024-08-13 15:14:14 -0700

    Update ports.sh

--- ports.sh
+++ ports.sh
@@ -1,2 +1,7 @@
 #!/bin/env
-echo "Doing stuff"
+
+export REDIRECT_URL=`gp url 3000`
+export FUSIONAUTH_URL=`gp url 9011`
+
+perl -pi -e 's/REACT_APP_FUSIONAUTH/${FUSIONAUTH_URL}/g' complete-application/src/main.tsx
+perl -pi -e 's/REACT_APP_REDIRECT/${REDIRECT_URL}/g' complete-application/src/main.tsx

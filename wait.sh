14c503e1f1a4631d47eadea313cf5bb82f4e69ad
Kirsten Hunter <77410784+synedra@users.noreply.github.com>
2024-08-14 10:21:36 -0700

    Update wait.sh

--- wait.sh
+++ wait.sh
@@ -1,5 +1,5 @@
 #!/bin/env
-
+echo "Waiting for the fusionauth server to start up on REACT_APP_FUSIONAUTH"
 until curl --output /dev/null --silent --head --fail REACT_APP_FUSIONAUTH; do
     printf '.'
     sleep 5

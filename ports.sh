e0c4c41dc92e8040f083d09a85b6572540971edd
Kirsten Hunter <77410784+synedra@users.noreply.github.com>
2024-08-14 10:18:30 -0700

    Update ports.sh

--- ports.sh
+++ ports.sh
@@ -4,4 +4,4 @@ export REDIRECT_URL=`gp url 3000`
 export FUSIONAUTH_URL=`gp url 9011`
 
 perl -pi -e 's/REACT_APP_FUSIONAUTH/$ENV{FUSIONAUTH_URL}/g' complete-application/src/main.tsx kickstart/kickstart.json
-perl -pi -e 's/REACT_APP_REDIRECT/$ENV{REDIRECT_URL}/g' complete-application/src/main.tsx kickstart/kickstart.json
+perl -pi -e 's/REACT_APP_REDIRECT/$ENV{REDIRECT_URL}/g' complete-application/src/main.tsx kickstart/kickstart.json wait.sh
